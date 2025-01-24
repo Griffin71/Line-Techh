dd53fa82a18a516417408d4e5218a61ffe431c8c
Griffin71 <ihavenoenemies1771@gmail.com>
2025-01-24 10:42:36 +0200

    contactme.php

--- contactme.php
+++ contactme.php
@@ -8,9 +8,20 @@
 
     $body = "<ul><li>Name: ".$name."</li><li>Phone: ".$phone."</li><li>Email: ".$email."</li><li>Message: ".$message."</li></ul>";
 
-    $status = mailfunction("", "Company", $body); //reciever
-    if($status)
-        echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
-    else
-        echo '<center><h1>Error sending message! Please try again.</h1></center>';    
+   // Define recipient email address
+$recipientEmail = "kabelosamkelo19@gmail.com"; // Replace with the actual recipient email
+
+// Prepare the subject for the email
+$subject = "New Contact Form Submission";
+
+// Send email using the mailfunction
+$status = mailfunction($recipientEmail, $subject, $body); // Send email to the recipient
+
+// Check if the email was sent successfully
+if ($status) {
+    echo '<center><h1>Thanks! We will contact you soon.</h1></center>';
+} else {
+    echo '<center><h1>Error sending message! Please try again.</h1></center>';
+}
+
 ?>
